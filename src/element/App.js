82b9e193da00311aff12d1ddf5243c7aca7eca24
morita mai <mai.20.04.ab@gmail.com>
2020-10-22 20:51:17 +0900

    ランダムで出た数字を保存し、経過を見えるようにリスト化。

--- src/element/App.js
+++ src/element/App.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import "../index.css";
 import Btn from "./Btn";
 
 const App = () => {
     // useStateで数字を保存する
+    const [hitNum, setHitNum] = useState([]);
+    const listItems = hitNum.map((number) =>
+    <li>{number}</li>
+    );
     return (
         <div className="container">
-            <h1>Hello! World.</h1>
-            <p>このページはテストページです。</p>
-            <Btn/>
-            {/* <DisplayNumber /> */}
+            <Btn setHitNum={setHitNum} hitNum={hitNum}/>
+            <ul>{listItems}</ul>
         </div>
     )
 }
--- src/element/Btn.js
+++ src/element/Btn.js
@@ -1,28 +1,16 @@
-import React, {useState} from "react";
+import React from "react";
 
-const Btn = () => {
+const Btn = ({ setHitNum, hitNum }) => {
     // useStateで作った数字を保存する関数を利用して数字を保存する
-    const [num, setNum] = useState(0);
     const handleClick = () => {
         const randomNumber = Math.floor( Math.random() * 75 + 1 ) ; //乱数の取得
         // useStateで作った数字を保存する関数を利用して数字を保存する
-        setNum(randomNumber);
+        setHitNum([...hitNum,randomNumber]);
     }
     return(
-        <>
             <button className="PushButton" onClick={handleClick}>START!</button>
-            <div>{num}</div>
-        </>
         /* useStateで作った数字を一旦表示する */
     )
 };
 
 export default Btn;
-
-    // 乱数のコードメモ
-    // const Random() => {
-    //     var num = Math.floor( Math.random() * 75 + 1 ) ; //乱数の取得
-    //     document.getElementById("hit-number").innerHTML = num; //乱数の出力
-    // }
-
-    // var a = Math.floor( Math.random() * 11 ) ;
